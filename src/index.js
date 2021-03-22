d52b61d36ffa943ad6de02d1ff576c7a16e85650
Nacu Florin <florin.nacu77@gmail.com>
2021-03-22 15:30:48 +0200

    added redux saga

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
+import createSagaMiddleware from "redux-saga";
 
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
@@ -11,6 +12,7 @@ import App from "./App";
 import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
+import { watchAuth, watchBurgerBuilder, watchOrder } from "./store/sagas/index";
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -20,7 +22,13 @@ const rootReducer = combineReducers({
     auth: authReducer,
 });
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, sagaMiddleware)));
+
+sagaMiddleware.run(watchAuth);
+sagaMiddleware.run(watchBurgerBuilder);
+sagaMiddleware.run(watchOrder);
 
 const app = (
     <Provider store={store}>
--- src/store/actions/burgerBuilder.js
+++ src/store/actions/burgerBuilder.js
@@ -1,5 +1,3 @@
-import axios from "../../axios-orders";
-
 import * as actionTypes from "./actionTypes";
 
 export const addIngredient = (name) => {
@@ -30,14 +28,7 @@ export const fetchIngredientsFailed = () => {
 };
 
 export const initIngredients = () => {
-    return (dispatch) => {
-        axios
-            .get("/ingredients.json")
-            .then((response) => {
-                dispatch(setIngredients(response.data));
-            })
-            .catch((error) => {
-                dispatch(fetchIngredientsFailed());
-            });
+    return {
+        type: actionTypes.INIT_INGREDIENTS,
     };
 };
